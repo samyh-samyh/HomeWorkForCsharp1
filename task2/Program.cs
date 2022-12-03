64c352d1665441385adcd44e43e0f386ec665e28
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 01:53:40 +0300

    2dTaskWasDone

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введи два числа: ");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"max= {a}, min= {b}");
+}
+else
+{
+    Console.WriteLine($"max= {b}, min= {a}");
+}
+if (a==b)
+{
+    Console.WriteLine("Числа одинаковы");
+}
