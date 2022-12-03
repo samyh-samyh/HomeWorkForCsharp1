efc13a24f63cd849513fbf09d8c59c9865b225c0
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 02:24:21 +0300

    4thTaskWasDone

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Напиши три числа через ENTER: ");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int c=int.Parse(Console.ReadLine());
+int m=0;
+if (a>b)
+{
+     m = a;
+}
+if (b>m)
+{
+     m = b;
+}
+if (c>m)
+{
+     m = c;
+}
+Console.WriteLine($"{m} является максимальным из трех введенных");
