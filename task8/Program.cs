2f9674cca6932557eadc095ae4e651f8d5d0e4b8
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 11:31:55 +0300

    task8done

---
+++ task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Ввести целое положительное число: ");
+int number=int.Parse(Console.ReadLine());
+int a=0;
+while (a<number-1)
+{
+    a=a+2;
+    Console.WriteLine(a);
+}
