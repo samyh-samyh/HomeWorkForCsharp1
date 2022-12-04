83caed8aa165b29a5672a831df0e53415a362e8f
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 20:57:55 +0300

    10done

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 10: Напишите программу, которая принимает
+// на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+
+Console.Clear();
+Console.WriteLine("Введи трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int second=number/10%10;
+Console.WriteLine($"{number} --> {second}");
