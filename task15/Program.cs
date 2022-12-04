e93a63eebf19003303f717754c3429e990a9a33e
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 21:29:45 +0300

    15done

---
+++ task15/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+
+Console.Clear();
+Console.WriteLine("Введи номер дня недели(от 1 до 7): ");
+int day=int.Parse(Console.ReadLine());
+if (day>5) Console.WriteLine("да");
+else Console.WriteLine("нет");
