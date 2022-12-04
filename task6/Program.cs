35fcee10cdfa7c295aef07c9b24ef5c9dfc4d3df
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 11:16:41 +0300

    task6done

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Ввести целое число: ");
+int a=int.Parse(Console.ReadLine());
+int b=a%2;
+if (b==0)
+{
+    Console.Write($"Число {a} четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} не четное");
+}
