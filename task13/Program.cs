ed20553d108f6b2c5397ae9fa771e7ccd6d9772f
Sergey Sidorov <89048598831s@gmail.com>
2022-12-04 21:12:40 +0300

    13done

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 10: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+
+
+Console.Clear();
+Console.WriteLine("Введи число: ");
+int number = int.Parse(Console.ReadLine());
+if (number>100)
+{
+    int second=number%10;
+    Console.WriteLine($"{number} --> третья цифра {second}");
+}
+else
+{
+     Console.WriteLine($"{number} --> третьей цифры нет");
+}
