a634122104a029e34bf1bcef829d2aa29909a601
Sergey Sidorov <89048598831s@gmail.com>
2022-12-08 19:29:58 +0300

    21&&19done

---
+++ task23/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n =int.Parse(Console.ReadLine());
+for (int i=1; i<=n; i++)
+{
+   Console.WriteLine(Math.Pow(i, 3));   
+}
+for (int j=-1; j>=n; j--)
+{
+   Console.WriteLine(Math.Pow(j, 3));   
+}
